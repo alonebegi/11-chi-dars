a2ea120aee156a420c4551fc0fc61606fe8dede8
Sunnatillo Azimjonov <monomusix001@gmail.com>
2024-03-19 23:06:23 +0300

    10 dars class

---
+++ app.js
@@ -0,0 +1,105 @@
+// class Study {
+//   constructor(who, where, clas, language) {
+//     this.who = who;
+//     this.where = where;
+//     this.clas = clas;
+//     this.language = language;
+//   }
+
+//   work() {
+//     // ...
+//   }
+
+//   studing() {
+//     // .,.
+//   }
+// }
+
+// let user1 = new Study("he", "fergana", "registan", "ENGLSH");
+
+// console.log(user1);
+
+// class All extends Study {
+//   constructor(name, age, work, hobbi) {
+//     super(name, age, work);
+//     this.hobbi = hobbi;
+//   }
+
+//   control() {
+//     // .,.
+//   }
+// }
+// let admin = new All("behruz", 17, "developer", "play game");
+// console.log(admin);
+
+// 1 masala
+// class Person {
+//   constructor(ism, yosh, mamlakat) {
+//     this.ism = ism;
+//     this.yosh = yosh;
+//     this.mamlakat = mamlakat;
+//   }
+// }
+
+// let user = new Person("bek", "17", "Uzbekistan");
+// console.log(user);
+
+// class All extends Person {
+//   constructor(name, age, work, hobbi) {
+//     super(name, age, work);
+//     this.hobbi = hobbi;
+//   }
+
+//   control() {
+//     // .,.
+//   }
+// }
+// let admin = new All("bek", 17, "developer", "play game");
+// console.log(admin);
+
+// 2masala
+// class rectengle {
+//   constructor(weight, hight) {
+//     this.weight = weight;
+//     this.hight = hight;
+//   }
+// }
+
+// let user1 = (50, 50);
+// console.log((user1 + user1) * 2);
+
+// masala 3
+// class Vehicle {
+//   constructor(yili, modeli, style) {
+//     this.yili = yili;
+//     this.modeli = modeli;
+//     this.style = style;
+//   }
+// }
+
+// let user2 = new Vehicle(2020, "Nexia 2", "Strong");
+// console.log(user2);
+
+// class Add extends Vehicle {
+//   constructor(name, work, hobbi) {
+//     super(name, work);
+//     this.hobbi = hobbi;
+//   }
+
+//   control() {
+//     // .,.
+//   }
+// }
+// let supre = new Add("Nexia 2", "drive", "legenda");
+// console.log(supre);
+
+// 5 masala
+// class shape {
+//   constructor(mediasi, balandligi) {
+//     this.mediasi = mediasi;
+//     this.balandligi = balandligi;
+//   }
+// }
+
+// let topsh = new shape(40, 70);
+// console.log((40 / 2) * 70);
